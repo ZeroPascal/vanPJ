e9eb7ff3f96b4c813753ba4a36df8b470c40eaab
flint <drew.t.winston@gmail.com>
2021-08-12 16:21:21 -0500

    Concurrency Fix

--- index.ts
+++ index.ts
@@ -210,16 +210,8 @@ io.on('connection', (socket: Socket) => {
     let activePJs = CommandPackage.pjIDs.map(pjID=>{
       return pjs.getPJ(pjID)
     })
-   // if(activePJs.length>1){
-    const { results, errors } = await PromisePool
-    .for(activePJs)
-  //  .withConcurrency(activePJs.length)
-    .process(async pj => {
-      await pj.Control(CommandPackage.cmd,CommandPackage.vartiable)
-    
-    })
- /* } else {
-    
+   
+    if(activePJs.length>1){
     const { results, errors } = await PromisePool
     .for(activePJs)
     .withConcurrency(activePJs.length)
@@ -227,7 +219,15 @@ io.on('connection', (socket: Socket) => {
       await pj.Control(CommandPackage.cmd,CommandPackage.vartiable)
     
     })
-  } */
+ } else {
+    
+    const { results, errors } = await PromisePool
+    .for(activePJs)
+    .process(async pj => {
+      await pj.Control(CommandPackage.cmd,CommandPackage.vartiable)
+    
+    })
+  } 
       pjs.updateStatus()
      // io.emit(ioCommands.EMITTING_PJS, pjs)
      // io.emit(ioCommands.EMITTING_STATUS, pjs.getStatus())
